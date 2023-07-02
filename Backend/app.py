69ddaa14787cb1882714ea2ca64243a7c963aeaa
fodare <folorunsodamilare007@hotmail.com>
2023-07-02 12:41:29 +0200

    Initial commit

---
+++ Backend/app.py
@@ -0,0 +1,76 @@
+import requests
+import platform
+import socket
+import os
+from flask import Flask, jsonify, make_response
+
+app = Flask(__name__)
+base_url = "https://randomuser.me/api/"
+session = requests.Session()
+
+random_user_url = "https://randomuser.me/api/?gender="
+
+
+@app.route("/")
+def health_check():
+    response_object = {
+        "Success": True,
+        "Message": platform.platform()
+    }
+    return make_response(jsonify(response_object), 200)
+
+
+@app.route("/backendservice/user")
+def get_random_user():
+    """Fetch random user from Random user api"""
+    r = session.get(base_url)
+    response_objects = {
+        "Success": False,
+        "Message": ""
+    }
+    if r.status_code == 200:
+        response_objects["Success"] = True
+        response_objects["Message"] = r.json()
+    else:
+        response_objects[
+            "Message"] = f'Error fetching user. Recived status code: {r.status_code}'
+    return make_response(jsonify(response_objects))
+
+
+@app.route("/backendservice/gender/<string:gender>")
+def get_user_by_gender(gender):
+    """Fetch random user based on gender"""
+    response_objects = {
+        "Success": False,
+        "Message": ""
+    }
+    r = session.get(f'{random_user_url}{gender}')
+    if r.status_code == 200:
+        response_objects["Success"] = True
+        response_objects["Message"] = r.json()
+    else:
+        response_objects["Message"] = "Error fetching random user."
+
+    return make_response(jsonify(response_objects))
+
+
+@app.route("/backendservice/user/count/<int:count>")
+def get_user_by_count(count):
+    """Fetch random user based on count"""
+    response_objects = {
+        "Success": False,
+        "Message": ""
+    }
+    r = session.get(f'{base_url}?results={count}')
+    if r.status_code == 200:
+        response_objects["Success"] = True
+        response_objects["Message"] = r.json()
+    else:
+        response_objects[
+            "Message"] = f'Error fetching user. Received respose code {r.status_code}'
+    return make_response(jsonify(response_objects))
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=True, host='0.0.0.0', port=port)
---
+++ frontend/app.py
@@ -0,0 +1,91 @@
+import platform
+import requests
+import datetime
+import os
+import socket
+from flask import Flask, jsonify, make_response
+
+app = Flask(__name__)
+base_url = "https://randomuser.me/api/"
+session = requests.Session()
+
+
+def get_host_details():
+    """Returns base url for the backend service"""
+    host_name = socket.gethostname()
+    host_ip_address = socket.getfqdn()
+    # base_url = os.environ['backend-service']
+    return str(host_name), str(host_ip_address)
+
+
+@app.route("/")
+def health_check():
+    response_object = {
+        "Success": True,
+        "Message": platform.platform()
+    }
+    return make_response(jsonify(response_object), 200)
+
+
+@app.route("/frontendservice/user")
+def fetch_users():
+    """
+    Calls the backend service to fetch random user.
+    """
+    response_object = {
+        "Success": False,
+        "Message": "message"
+    }
+    r = session.get('http://backendservice:5001/backendservice/user')
+    if r.status_code == 200:
+        response_object["Success"] = True
+        response_object["Message"] = r.json()
+    else:
+        response_object["Message"] = r.json()
+    return make_response(jsonify(response_object))
+
+
+@app.route("/frontendservice/user/gender/<string:gender>")
+def fetch_random_user_by_gender(gender):
+    """Fetch a user based on gender"""
+    hostName, Ip = get_host_details()
+    print(f'Machine I p address: {Ip}')
+    session.trust_env = False
+    r = session.get(
+        f'http://backendservice:5001/backendservice/gender/{gender}')
+    response_object = {
+        "Success": False,
+        "Message": "message"
+    }
+    if r.status_code == 200:
+        response_object["Success"] = True
+        response_object["Message"] = r.json()
+    else:
+        response_object["Message"] = "Error fetching user from backend service"
+
+    return make_response(jsonify(response_object))
+
+
+@app.route("/frontendservice/user/<int:count>")
+def fetch_random_user_count(count):
+    """Fetch user based on count from the backend service"""
+    response_objects = {
+        "Success": False,
+        "Message": ""
+    }
+    r = session.get(
+        f'http://backendservice:5001/backendservice/user/count/{count}')
+    if r.status_code == 200:
+        response_objects["Message"] = r.json()
+        response_objects["Success"] = True
+    else:
+        response_objects["Message"] = r.json()
+
+    return make_response(jsonify(response_objects))
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
+    # Supporting document
