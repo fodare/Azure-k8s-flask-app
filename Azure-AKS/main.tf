690d8556c1dfd75db6d5763e630b56d7d3b28119
fodare <folorunsodamilare007@hotmail.com>
2023-07-04 12:39:59 +0200

    Adding  config for app gateway.

--- Azure-AKS/main.tf
+++ Azure-AKS/main.tf
@@ -1,12 +1,22 @@
-# Create a resource group where the cluster.
-resource "azurerm_resource_group" "resource_group" {
-  name     = "${var.resource_group}_${var.environment}"
-  location = var.location
+# Terraform state stored in a backend.
+terraform {
+  backend "azurerm" {
+    # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
+    # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
+    # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
+    # container_name="<<storage_account_container_name>>" #OVERRIDE in TERRAFORM init
+  }
 }
 
 provider "azurerm" {
   //version = "~>2.0.0"
   features {}
+}
+
+# Create a resource group for the cluster.
+resource "azurerm_resource_group" "resource_group" {
+  name     = "${var.resource_group}_${var.environment}"
+  location = var.location
 }
 
 #  Create cluster
@@ -28,6 +38,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
     name       = "agentpool"
     node_count = var.node_count
     vm_size    = "standard_b2ms"
+    vnet_subnet_id = data.azurerm_subnet.kubesubnet.id
     # vm_size         = "standard_d2as_v5"      CHANGE IF AN ERROR ARISES 
   }
 
@@ -36,56 +47,121 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
     client_secret = var.client_secret
   }
 
+   network_profile {
+    network_plugin     = "azure"
+    dns_service_ip     = var.aks_dns_service_ip
+    docker_bridge_cidr = var.aks_docker_bridge_cidr
+    service_cidr       = var.aks_service_cidr
+  }
+
   tags = {
     Environment = var.environment
   }
+  depends_on = [ azurerm_virtual_network.testnetwork, azurerm_application_gateway.appgateway ]
 }
 
-# Ingress configuration
-# resource "kubernetes_ingress_v1" "defaultingres" {
-#   metadata {
-#     name      = "test-ingress"
-#     namespace = "default"
-#     annotations = {
-#       "nginx.ingress.kubernetes.io/rewrite-target" = "/$1"
-#     }
-#   }
-#   spec {
-#     rule {
-#       http {
-#         path {
-#           path = "/frontendservice/*"
-#           backend {
-#             service {
-#               name = "frontendservice"
-#               port {
-#                 number = 5000
-#               }
-#             }
-#           }
-#         }
-#         path {
-#           path = "/backendservice/*"
-#           backend {
-#             service {
-#               name = "backendservice"
-#               port {
-#                 number = 5001
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
+# Virtual network
+resource "azurerm_virtual_network" "testnetwork"{
+  name = var.virtual_network_name
+  location = var.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  address_space = [var.virtual_network_address_prefix]
 
-# Terraform state stored in a backend.
-terraform {
-  backend "azurerm" {
-    # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
-    # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
-    # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
-    # container_name="<<storage_account_container_name>>" #OVERRIDE in TERRAFORM init
+  subnet {
+    name = var.aks_subnet_name
+    address_prefix = var.aks_subnet_address_prefix
   }
+
+  subnet {
+    name = "appgwsubet"
+    address_prefix = var.app_gateway_subnet_address_prefix
+  }
+
+  tags {
+    enviroment = var.environment
+  }
+}
+
+
+resource "azurerm_public_ip" "publicip" {
+  name = "publicip"
+  location = var.location
+  resource_group_name = var.azurerm_resource_group.resource_group.name
+  allocation_method = "Static"
+  sku = "Standard"
+  tags = {
+    enviroment = var.environment
+  }
+}
+
+data "azurerm_subnet" "appgwsubnet" {
+  name                 = "appgwsubnet"
+  virtual_network_name = azurerm_virtual_network.testnetwork.name
+  resource_group_name  = azurerm_resource_group.resource_group.name
+  depends_on           = [azurerm_virtual_network.testnetwork]
+}
+
+data "azurerm_subnet" "kubesubnet" {
+  name                 = var.aks_subnet_name
+  virtual_network_name = azurerm_virtual_network.testnetwork.name
+  resource_group_name  = azurerm_resource_group.resource_group.name
+  depends_on           = [azurerm_virtual_network.testnetwork]
+}
+
+
+resource "azurerm_application_gateway" "appgateway" {
+  name = var.app_gateway_name
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location = var.location
+  sku {
+    name = var.app_gateway_sku
+    tier = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name = "appGatewayIpConfig"
+    subnet_id = data.azurerm_subnet.appgwsubnet.id
+  }
+
+  frontend_port {
+    name = "${azurerm_virtual_network.testnetwork.name}-feport"
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name = "${azurerm_virtual_network.testnetwork.name}-feip"
+    public_ip_address_id = azurerm_public_ip.publicip.id
+  }
+
+  backend_address_pool {
+    name = "${azurerm_virtual_network.testnetwork.name}-beap"
+  }
+
+  backend_http_settings {
+    name = "${azurerm_virtual_network.testnetwork.name}-be-htst"
+    cookie_based_affinity = "Disabled"
+    port = 80
+    protocol = "Http"
+    request_timeout = 1
+  }
+
+  http_listener {
+    name = "${azurerm_virtual_network.testnetwork.name}-httplstn"
+    frontend_ip_configuration = "${azurerm_virtual_network.testnetwork.name}-feip"
+    frontend_port_name = "${azurerm_virtual_network.testnetwork.name}-feport"
+    protocol = "Http"
+  }
+  
+  request_routing_rule {
+    name = "${azurerm_virtual_network.testnetwork.name}-rqrt"
+    rule_type = "Basic"
+    http_listener_name = "${azurerm_virtual_network.testnetwork.name}-httplstn"
+    backend_address_pool = "${azurerm_virtual_network.testnetwork.name}-beap"
+    backend_http_settings_name = "${azurerm_virtual_network.testnetwork.name}-be-htst"
+  }
+  tags  {
+    enviroment = var.environment
+  }
+  depends_on = [ azurerm_virtual_network.testnetwork, azurerm_public_ip.publicip ]
 }
--- Azure-AKS/variables.tf
+++ Azure-AKS/variables.tf
@@ -25,3 +25,56 @@ variable "cluster_name" {
 variable "resource_group" {
   default = "kubernetes"
 }
+
+
+# Ingress configuration start here
+
+variable "virtual_network _name"{
+  description = "Virtual network name"
+  default = "aksVirtualNetwork"
+}
+
+variable "virtual_network_address_prefix" {
+  description = "VNET address prefix"
+  default     = "192.168.0.0/16"
+}
+
+variable "aks_subnet_name" {
+  description = "Subnet Name."
+  default     = "kubesubnet"
+}
+
+variable "aks_subnet_address_prefix" {
+  description = "Subnet address prefix."
+  default     = "192.168.0.0/24"
+}
+
+variable "app_gateway_subnet_address_prefix" {
+  description = "Subnet server IP address."
+  default     = "192.168.1.0/24"
+}
+
+variable "app_gateway_name" {
+  description = "Name of the Application Gateway"
+  default     = "ApplicationGatewayDev"
+}
+
+variable "app_gateway_sku" {
+  description = "Name of the Application Gateway SKU"
+  default     = "Standard_v2"
+}
+
+variable "aks_dns_service_ip" {
+  description = "DNS server IP address"
+  default     = "10.0.0.10"
+}
+
+variable "aks_docker_bridge_cidr" {
+  description = "CIDR notation IP for Docker bridge."
+  default     = "172.17.0.1/16"
+}
+
+variable "aks_service_cidr" {
+  description = "CIDR notation IP range from which to assign service cluster IPs"
+  default     = "10.0.0.0/16"
+}
